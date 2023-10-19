cadc2e5bb98b36169468772d9c64df79c2ec4f83
vinser <vinser@outlook.com>
2023-10-19 07:25:50 +0500

    Refactor for HeaderTable

--- m_listui.go
+++ m_listui.go
@@ -21,33 +21,32 @@ const (
 var orderSymbols = []string{" ", " ↓", " ↑"}
 
 func (a *App) newListView() {
-	a.listHeaders = []*ActiveHeader{
+	a.listColumns = []*ListCell{
 		{Name: "File Name", Order: orderAsc, SortAsc: a.orderByFileNameAsc, SortDesc: a.orderByFileNameDesc},
 		{Name: "Exif Date"},
 		{Name: "File Date", SortAsc: a.orderByFileDateAsc, SortDesc: a.orderByFileDateDesc},
 		{Name: "Entered Date"},
 		{Name: "Dropped"},
 	}
-	a.listColumnsNum = len(a.listHeaders)
 
-	a.listHeaders[0].Width = labelMaxWidth(a.listHeaders[0].Name, DisplyDateFormat, FileNameDateFormat+".000")
-	a.listHeaders[1].Width = labelMaxWidth(a.listHeaders[1].Name, DisplyDateFormat)
-	a.listHeaders[2].Width = labelMaxWidth(a.listHeaders[2].Name, DisplyDateFormat)
-	a.listHeaders[3].Width = labelMaxWidth(a.listHeaders[3].Name, DisplyDateFormat)
-	a.listHeaders[4].Width = labelMaxWidth(a.listHeaders[4].Name)
+	a.listColumns[0].Width = columnWidth(a.listColumns[0].Name, DisplayDateFormat, FileNameDateFormat+".000")
+	a.listColumns[1].Width = columnWidth(a.listColumns[1].Name, DisplayDateFormat)
+	a.listColumns[2].Width = columnWidth(a.listColumns[2].Name, DisplayDateFormat)
+	a.listColumns[3].Width = columnWidth(a.listColumns[3].Name, DisplayDateFormat)
+	a.listColumns[4].Width = columnWidth(a.listColumns[4].Name)
 
 	a.listTable = widget.NewTableWithHeaders(a.dataLength, a.dataCreate, a.dataUpdate)
 	a.listTable.CreateHeader = a.headerCreate
 	a.listTable.UpdateHeader = a.headerUpdate
 
-	for i := 0; i < len(a.listHeaders); i++ {
-		a.listTable.SetColumnWidth(i, a.listHeaders[i].Width)
+	for i := 0; i < len(a.listColumns); i++ {
+		a.listTable.SetColumnWidth(i, a.listColumns[i].Width)
 	}
 	bottomCount := widget.NewLabel(fmt.Sprintf("total: %d", len(a.List)))
 	a.listView = container.NewBorder(nil, bottomCount, nil, nil, a.listTable)
 }
 
-func labelMaxWidth(labels ...string) float32 {
+func columnWidth(labels ...string) float32 {
 	width := widget.NewLabel("").MinSize().Width
 	re := regexp.MustCompile(`\w`)
 	for _, l := range labels {
@@ -59,49 +58,55 @@ func labelMaxWidth(labels ...string) float32 {
 	return width
 }
 
-type ActiveCell struct {
+type ListCell struct {
 	widget.Label
+	Name     string
+	Width    float32
+	Order    int
 	OnTapped func()
+	SortAsc  func(i, j int) bool
+	SortDesc func(i, j int) bool
 }
 
-func newActiveCell(label string) *ActiveCell {
-	c := &ActiveCell{}
-	c.ExtendBaseWidget(c)
-	c.Label.SetText(label)
-	c.Truncation = fyne.TextTruncateEllipsis
-	return c
+func newListCell(label string) *ListCell {
+	h := &ListCell{}
+	h.ExtendBaseWidget(h)
+	h.Label.SetText(label)
+	return h
 }
 
-func (h *ActiveCell) Tapped(_ *fyne.PointEvent) {
-	if h.OnTapped != nil {
-		h.OnTapped()
+func (c *ListCell) Tapped(_ *fyne.PointEvent) {
+	if c.OnTapped != nil {
+		c.OnTapped()
 	}
 }
 
-func (h *ActiveCell) TappedSecondary(_ *fyne.PointEvent) {
+func (c *ListCell) TappedSecondary(_ *fyne.PointEvent) {
 }
+
 func (a *App) dataLength() (rows int, cols int) {
-	return len(a.List), a.listColumnsNum
+	return len(a.List), len(a.listColumns)
 }
 
 func (a *App) dataCreate() fyne.CanvasObject {
-	data := newActiveCell(DisplyDateFormat)
+	data := newListCell(DisplayDateFormat)
+	data.Truncation = fyne.TextTruncateEllipsis
 	return data
-
 }
+
 func (a *App) dataUpdate(id widget.TableCellID, o fyne.CanvasObject) {
 	if id.Row == -1 {
 		return
 	}
 	text := ""
 	photo := a.List[id.Row]
-	data := o.(*ActiveCell)
+	data := o.(*ListCell)
 	switch id.Col {
 	case 0:
 		text = filepath.Base(photo.File)
 		data.TextStyle.Bold = false
 	case 1, 2, 3:
-		text = photo.Dates[id.Col-1]
+		text = listDateToDisplayDate(photo.Dates[id.Col-1])
 		if id.Col-1 == photo.DateUsed {
 			data.TextStyle.Bold = true
 		} else {
@@ -118,63 +123,36 @@ func (a *App) dataUpdate(id widget.TableCellID, o fyne.CanvasObject) {
 		a.scrollFrame(id.Row)
 		a.toggleView()
 	}
-
-}
-
-type ActiveHeader struct {
-	widget.Label
-	Name     string
-	Width    float32
-	Order    int
-	OnTapped func()
-	SortAsc  func(i, j int) bool
-	SortDesc func(i, j int) bool
-}
-
-func newActiveHeader(label string) *ActiveHeader {
-	h := &ActiveHeader{}
-	h.ExtendBaseWidget(h)
-	h.Label.SetText(label)
-	return h
-}
-
-func (h *ActiveHeader) Tapped(_ *fyne.PointEvent) {
-	if h.OnTapped != nil {
-		h.OnTapped()
-	}
-}
-
-func (h *ActiveHeader) TappedSecondary(_ *fyne.PointEvent) {
 }
 
 func (a *App) headerCreate() fyne.CanvasObject {
-	header := newActiveHeader("000")
+	header := newListCell("000")
 	return header
 
 }
 func (a *App) headerUpdate(id widget.TableCellID, o fyne.CanvasObject) {
-	header := o.(*ActiveHeader)
+	header := o.(*ListCell)
 	header.TextStyle.Bold = true
 	if id.Col == -1 {
 		header.Label.SetText(strconv.Itoa(id.Row + 1))
 		return
 	} else {
-		header.Label.SetText(a.listHeaders[id.Col].Name + orderSymbols[a.listHeaders[id.Col].Order])
-		if a.listHeaders[id.Col].SortAsc == nil && a.listHeaders[id.Col].SortDesc == nil {
+		header.Label.SetText(a.listColumns[id.Col].Name + orderSymbols[a.listColumns[id.Col].Order])
+		if a.listColumns[id.Col].SortAsc == nil && a.listColumns[id.Col].SortDesc == nil {
 			header.TextStyle.Italic = true
 			return
 		}
 	}
 	header.OnTapped = func() {
-		for j, h := range a.listHeaders {
+		for j, h := range a.listColumns {
 			if j == id.Col {
 				switch h.Order {
 				case unordered, orderDesc:
 					h.Order = orderAsc
-					a.reorderList(a.listHeaders[id.Col].SortAsc)
+					a.reorderList(a.listColumns[id.Col].SortAsc)
 				case orderAsc:
 					h.Order = orderDesc
-					a.reorderList(a.listHeaders[id.Col].SortDesc)
+					a.reorderList(a.listColumns[id.Col].SortDesc)
 				}
 				continue
 			} else {
